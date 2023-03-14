1f79d3d84807b94a3dd9c86310cf5a139769c934
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-03-13 22:56:35 -0600

    add exercise requirements to each file, finish first exercise

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,10 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+# Borrowing and modifying the code from Exercise one, create 3 more stores:
+# Surrey (annual_revenue of 224000, carries women's apparel only)
+# Whistler (annual_revenue of 1900000 carries men's apparel only)
+# Yaletown (annual_revenue of 430000 carries men's and women's apparel)
+# Using the where class method from Active Record, fetch (a collection of) only those stores that carry men's apparel. Assign the results to a variable @mens_stores.
+# Loop through each of these stores and output their name and annual revenue on each line.
+# Do another fetch but this time load stores that carry women's apparel and are generating less than $1M in annual revenue.
